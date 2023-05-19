ba8bb183e8fe28a64ef0b9479f76b024067b0267
cha <cdm2883@gmail.com>
2023-05-20 00:19:44 +0900

    commit

--- src/Bank_singleton_innerClass/AppMain.java
+++ src/Bank_singleton_innerClass/AppMain.java
@@ -5,15 +5,15 @@ public class AppMain {
 
         Transaction tr = new Transaction();
         int accountNum1  = tr.openAccount(0);
-        int accountNum2 = tr.openAccount(0);
 
         tr.deposit(accountNum1,1000);
-        tr.deposit(accountNum2, 2000);
+
         tr.withdraw(accountNum1, 300);
+
         tr.getAccountHistory(accountNum1);
 
         tr.checkBalance(accountNum1);
-        tr.checkBalance(accountNum2);
+
 
     }
 }
